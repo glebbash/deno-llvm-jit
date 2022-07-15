dc73a8a067a73274891f8c6eeffb84ca54b134a1
glebbash <glebbash@gmail.com>
2022-07-15 17:55:51 +0000

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # deno-llvm-jit
 Deno LLVM JIT example
+
+## Running example
+
+```sh
+deno run -A --unstable sum.ts
+```
+
+## Running C example
+
+```sh
+make && ./sum
+```
+
+## How libLLVM-15git.so was built
+
+* Warning: Building this takes a really long time (1-2h).
+
+```sh
+git clone https://github.com/llvm/llvm-project.git
+```
+
+```sh
+cmake \
+  -DCMAKE_INSTALL_PREFIX="../llvm-out" \
+  -DCMAKE_BUILD_TYPE="MinSizeRel" \
+  -DLLVM_TARGETS_TO_BUILD="X86;WebAssembly" \
+  -DLLVM_BUILD_TOOLS="NO" \
+  -DLLVM_BUILD_LLVM_DYLIB="YES" \
+  ../llvm
+```
+
+```sh
+cmake --build . --target install
+```
---
+++ sum.ts
@@ -0,0 +1,168 @@
+const ptr = "pointer";
+const LLVMAbortProcessAction = 0;
+
+const llvm = loadLLVM();
+
+const mod = llvm.LLVMModuleCreateWithName(cstr("my_module"));
+
+const param_types = [llvm.LLVMInt32Type(), llvm.LLVMInt32Type()];
+const ret_type = llvm.LLVMFunctionType(
+  llvm.LLVMInt32Type(),
+  ptrs(param_types),
+  2,
+  0,
+);
+const sum = llvm.LLVMAddFunction(mod, cstr("sum"), ret_type);
+
+const entry = llvm.LLVMAppendBasicBlock(sum, cstr("entry"));
+
+const builder = llvm.LLVMCreateBuilder();
+llvm.LLVMPositionBuilderAtEnd(builder, entry);
+const tmp = llvm.LLVMBuildAdd(
+  builder,
+  llvm.LLVMGetParam(sum, 0),
+  llvm.LLVMGetParam(sum, 1),
+  cstr("tmp"),
+);
+llvm.LLVMBuildRet(builder, tmp);
+
+let error = new BigUint64Array(1);
+llvm.LLVMVerifyModule(mod, LLVMAbortProcessAction, error);
+llvm.LLVMDisposeMessage(error[0]);
+
+llvm.LLVMDumpModule(mod);
+
+const engine = new BigUint64Array(1);
+error = new BigUint64Array(1);
+
+llvm.LLVMLinkInMCJIT();
+llvm.LLVMInitializeX86Target();
+llvm.LLVMInitializeX86AsmPrinter();
+if (llvm.LLVMCreateExecutionEngineForModule(engine, mod, error) != 0) {
+  console.error("failed to create execution engine");
+  Deno.exit();
+}
+if (error[0]) {
+  console.error("error:", fromCString(error[0]));
+  llvm.LLVMDisposeMessage(error[0]);
+  Deno.exit(1);
+}
+
+const sum_func = new Deno.UnsafeFnPointer(
+  llvm.LLVMGetFunctionAddress(engine[0], cstr("sum")),
+  {
+    parameters: ["i32", "i32"],
+    result: "i32",
+  },
+);
+console.log(sum_func.call(34, 35));
+
+llvm.LLVMDisposeBuilder(builder);
+llvm.LLVMDisposeExecutionEngine(engine);
+
+// utils
+
+function cstr(str: string) {
+  return Uint8Array.from(toCharCodes(str + "\0"));
+}
+
+function toCharCodes(str: string): number[] {
+  return [...str].map((_, i) => str.charCodeAt(i));
+}
+
+function fromCString(pointer: bigint) {
+  return new Deno.UnsafePointerView(pointer).getCString();
+}
+
+function ptrs(pointers: bigint[]) {
+  return Deno.UnsafePointer.of(
+    BigInt64Array.from(pointers),
+  );
+}
+
+function loadLLVM() {
+  const lib = Deno.dlopen("./libLLVM-15git.so", {
+    LLVMModuleCreateWithName: {
+      parameters: [ptr],
+      result: ptr,
+    },
+    LLVMInt32Type: {
+      parameters: [],
+      result: ptr,
+    },
+    LLVMFunctionType: {
+      parameters: [ptr, ptr, "i32", "i32"],
+      result: ptr,
+    },
+    LLVMAddFunction: {
+      parameters: [ptr, ptr, ptr],
+      result: ptr,
+    },
+    LLVMAppendBasicBlock: {
+      parameters: [ptr, ptr],
+      result: ptr,
+    },
+    LLVMCreateBuilder: {
+      parameters: [],
+      result: ptr,
+    },
+    LLVMPositionBuilderAtEnd: {
+      parameters: [ptr, ptr],
+      result: ptr,
+    },
+    LLVMBuildAdd: {
+      parameters: [ptr, ptr, ptr, ptr],
+      result: ptr,
+    },
+    LLVMGetParam: {
+      parameters: [ptr, "i32"],
+      result: ptr,
+    },
+    LLVMBuildRet: {
+      parameters: [ptr, ptr],
+      result: ptr,
+    },
+    LLVMVerifyModule: {
+      parameters: [ptr, "i32", ptr],
+      result: ptr,
+    },
+    LLVMDisposeMessage: {
+      parameters: [ptr],
+      result: "void",
+    },
+    LLVMDumpModule: {
+      parameters: [ptr],
+      result: "void",
+    },
+    LLVMLinkInMCJIT: {
+      parameters: [],
+      result: "void",
+    },
+    LLVMInitializeX86Target: {
+      parameters: [],
+      result: "i32",
+    },
+    LLVMInitializeX86AsmPrinter: {
+      parameters: [],
+      result: "i32",
+    },
+    LLVMCreateExecutionEngineForModule: {
+      parameters: [ptr, ptr, ptr],
+      result: "i32",
+    },
+    LLVMGetFunctionAddress: {
+      parameters: [ptr, ptr],
+      result: "function",
+    },
+    LLVMDisposeBuilder: {
+      parameters: [ptr],
+      result: "void",
+    },
+    LLVMDisposeExecutionEngine: {
+      parameters: [ptr],
+      result: "void",
+    },
+  });
+
+  return lib.symbols;
+}
